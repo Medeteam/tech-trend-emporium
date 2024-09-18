8fa9e40aa40dd7fd8bcc4ce6faf22ec4459b8a1f
Jhonatan Tamayo Morales <Jhonatan.Tamayo@endava.com>
2024-09-18 15:22:35 -0500

    Added Login Controller and DTO for authentication

---
+++ emporium/app/Controllers/LoginController.cs
@@ -0,0 +1,74 @@
+﻿using Data.Entities;
+using Data.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using DotNetEnv;
+using System.Security.Claims;
+using Data;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DBContextTechEmporiumTrend _context;
+        private readonly string _jwtKey;
+        private readonly string _jwtIssuer;
+
+        public LoginController(DBContextTechEmporiumTrend context)
+        {
+            _context = context;
+            DotNetEnv.Env.Load();
+            _jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
+            _jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserLoginDto userLogin)
+        {
+            var user = AuthenticateUser(userLogin);
+
+            if (user != null)
+            {
+                var token = GenerateJWTToken(user);
+                return Ok(token);
+            }
+
+            return NotFound("User not found");
+        }
+
+        private string GenerateJWTToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim(ClaimTypes.Sid , user.User_id.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.RoleName)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtIssuer,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private User? AuthenticateUser(UserLoginDto userLogin)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == userLogin.Username 
+                && u.Password == userLogin.Password);
+            return user is not null ? user : null;
+        }
+    }
+}
--- emporium/app/Program.cs
+++ emporium/app/Program.cs
@@ -1,18 +1,61 @@
 
+<<<<<<< Updated upstream
+=======
+using Data;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+>>>>>>> Stashed changes
 namespace app
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
+<<<<<<< Updated upstream
+=======
+            DotNetEnv.Env.Load();
+
+            string connectionString = Environment.GetEnvironmentVariable("SQLSERVER_CONNECTION_STRING");
+            string jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
+            string jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+
+
+            builder.Services.AddDbContext<DBContextTechEmporiumTrend>(
+                options=>options.UseSqlServer(connectionString)
+                
+                );
+
+>>>>>>> Stashed changes
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Add authentication service
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtIssuer,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                    };
+                });
+
+            builder.Services.AddAuthorization();
 
             var app = builder.Build();
 
