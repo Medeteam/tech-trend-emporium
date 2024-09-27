72bbfed16d67de3482533eaeb0b6a5b6c820506f
Jhonatan Tamayo Morales <Jhonatan.Tamayo@endava.com>
2024-09-27 10:45:23 -0500

    Added namespace for ApiProductDto

--- emporium/Data/DTOs/ApiProductDto.cs
+++ emporium/Data/DTOs/ApiProductDto.cs
@@ -1,8 +1,12 @@
-﻿public class ApiProductDto
+﻿
+namespace Data.DTOs
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Image { get; set; }
-    public decimal Price { get; set; }
-    public string Category { get; set; }
+    public class ApiProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
 }
