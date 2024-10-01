c50870bd9b6d191598da68546eb7f64222444d81
sebasarias17 <ariasusmasebastian@gmail.com>
2024-10-01 11:32:38 -0500

    Coupon Controller Created

---
+++ emporium/Data/DTOs/CouponDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DTOs
+{
+    public class CreateCouponDto
+    {
+        public string Coupon_name { get; set; }
+        public int Discount { get; set; }
+        public string Code { get; set; }
+        public bool Coupon_status { get; set; }
+    }
+}
