9d035380c70f73aa8337cea3243b22426daa1fae
Jhonatan Tamayo Morales <Jhonatan.Tamayo@endava.com>
2024-10-07 12:10:42 -0500

    Updated wishlists verifications

---
+++ emporium/Data/DTOs/WishListDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DTOs
+{
+    public class WishListDto
+    {
+        public Guid userId { get; set; }
+        public List<Guid> productList { get; set; }
+    }
+}
