17217a12c647aba774753b8ed2b8529177d7d3ab
sebasarias17 <ariasusmasebastian@gmail.com>
2024-10-21 22:04:30 -0500

    product updated for front

--- emporium/Data/DTOs/ProductCartDto.cs
+++ emporium/Data/DTOs/ProductCartDto.cs
@@ -13,5 +13,7 @@ namespace Data.DTOs
         public decimal price { get; set; }
         public string image { get; set; }
         public int quantity { get; set; }
+
+        public string description { get; set; }
     }
 }
